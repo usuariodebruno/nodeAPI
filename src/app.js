5a8a260eb1fa63d71a4da37f3008954317a20df5
usuariodebruno <usuariodebruno@gmail.com>
2024-11-06 16:55:05 -0300

    Melhorando e tratando erros

--- src/app.js
+++ src/app.js
@@ -3,9 +3,29 @@ const app = express();
 
 const rotaProdutos = require('./routes/produtos');
 const rotaPedidos = require('./routes/pedidos');
+const morgan = require('morgan');
+
+app.use(morgan('dev'));
 
 app.use('/produtos', rotaProdutos);
-app.use('/pedidos', rotaPedidos )
+app.use('/pedidos', rotaPedidos );
 
+// Quando não encontra a rota, entra aqui: 
+app.use((req, res, next) => {
+    const erro = new Error('Rota não encontrada')
+    erro.status = 404;
+    next(erro);
+});
+
+// Tratamento de erro 
+app.use((error, req, res, next,) => {
+    res.status(error.status || 500)
+    return res.send({
+        erro: {
+            mensagem: error.message,
+            status: error.status
+        }
+    });    
+});
 
 module.exports = app;
